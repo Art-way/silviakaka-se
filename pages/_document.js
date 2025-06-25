c1fed7c23befb32cebf2513b2c5b42d848406dc5
subhehmoden <hmed2sbhe@gmail.com>
2025-06-25 18:02:27 +0300

    add ads.txt google tag

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,42 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="sv">
-        <Head />
+        <Head>
+          {/* Google Ads */}
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9653005764752290"
+            crossOrigin="anonymous"
+          ></script>
+          <script
+            async
+            src="https://d3u598arehftfk.cloudfront.net/prebid_hb_14406_23209.js"
+          ></script>
+          {/* Google Tag Manager */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-THT22J2M');`,
+            }}
+          />
+        </Head>
         <body>
+          {/* Google Tag Manager (noscript) */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-THT22J2M"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
+
           <Main />
           <NextScript />
         </body>
